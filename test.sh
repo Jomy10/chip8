9939d0226f3880e29c3d287995f64fe3ac4064b4
Jomy10 <jonas.vbs4@gmail.com>
2023-08-20 13:02:20 +0200

    added tests

---
+++ test.sh
@@ -0,0 +1,24 @@
+zig build -Dexe-type=cli -Dplatform=$1 || exit
+
+INFO='\033[0;36m'
+CLEAR='\033[0m'
+
+scale=10
+frate=840 # fast tests
+tests=tests/bin/*.ch8
+
+info() {
+  printf "${INFO}$1${CLEAR}\n"
+}
+
+test_info() {
+  echo "running $1"
+  [[ "$1" =~ .*3.* ]] && info "Check if all checkmarks are present"
+  [[ "$1" =~ .*4.* ]] && info "Check if all checkmarks are present"
+}
+
+for test in tests/bin/*.ch8
+do
+  test_info $test
+  ./zig-out/bin/chip8 $test $scale $frate
+done
